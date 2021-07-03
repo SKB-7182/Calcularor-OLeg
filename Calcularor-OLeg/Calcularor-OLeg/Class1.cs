24f43db281577d6444469c83f7be1e1ae06bfa5a
SKB-7182 <86765594+SKB-7182@users.noreply.github.com>
2021-07-03 19:58:41 +0600

    Summary

--- Calcularor-OLeg/Calcularor-OLeg/Class1.cs
+++ Calcularor-OLeg/Calcularor-OLeg/Class1.cs
@@ -1,11 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Calcularor_OLeg
 {
-    class Class1
+    static class Class1
     {
+        public static calculator2 cal1(int count)
+        {
+            switch (count)
+            {
+                case 1:
+                    return new Plus();
+                case 2:
+                    return new Minus();
+                case 3:
+                    return new Myltiply();
+                case 4:
+                    return new division();
+                default:
+                    return new Plus();
+            }
+
+
+        }
+    }
+
+    interface calculator2
+    {
+        double calculate(double a, double b);
+    }
+
+    class Plus : calculator2
+    {
+        public double calculate(double firstValue, double secendValue)
+        {
+            return firstValue + secendValue;
+        }
 
     }
+
+    class Minus : calculator2
+    {
+        public double calculate(double firstValue, double secendValue)
+        {
+            return firstValue - secendValue;
+        }
+    }
+    class Myltiply : calculator2
+    {
+        public double calculate(double firstValue, double secendValue)
+        {
+            return firstValue * secendValue;
+        }
+    }
+    class division : calculator2
+    {
+        public double calculate(double firstValue, double secendValue)
+        {
+            return firstValue / secendValue;
+        }
+    }
+
+    static class Class2
+    {
+        public static calculator1 cal2(int count)
+        {
+            switch (count)
+            {
+                case 5:
+                    return new power();
+                default:
+                    return new power();
+            }
+
+        }
+    }
+    interface calculator1
+    {
+        double calculate(double fitrsValue);
+    }
+
+    class power : calculator1
+    {
+        public double calculate(double firstValue)
+        {
+            return Math.Pow(2,firstValue);
+        }
+    }
+
+
+
 }
--- Calcularor-OLeg/Calcularor-OLeg/Form1.cs
+++ Calcularor-OLeg/Calcularor-OLeg/Form1.cs
@@ -20,109 +20,69 @@ namespace Calcularor_OLeg
         private double a, b;
         private int count;
         bool znak = true;
-
-        private void calculate()
-        {
-            switch (count)
-            {
-                case 1:
-                    b = a + float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-
-                case 2:
-                    b = a - float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                case 3:
-                    b = a * float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                case 4:
-                    b = a / float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                default:
-                    break;
-            }
-        }
+        private double result;
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
         private void button10_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 0;
         }
-
         private void button7_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 1;
         }
-
         private void button8_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 2;
         }
-
         private void button9_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 3;
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 4;
         }
-
         private void button5_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 5;
         }
-
         private void button6_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 6;
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 7;
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 8;
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + 9;
         }
-
         private void button11_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + ",";
         }
-
         private void button19_Click(object sender, EventArgs e)
         {
             textBox1.Text = "";
             label1.Text = "";
         }
-
         private void button18_Click(object sender, EventArgs e)
         {
             int lenght = textBox1.Text.Length - 1;
@@ -146,7 +106,6 @@ namespace Calcularor_OLeg
                 znak = true;
             }
         }
-
         private void button13_Click(object sender, EventArgs e)
         {
             if (textBox1.Text != "")
@@ -159,7 +118,6 @@ namespace Calcularor_OLeg
 
             }
         }
-
         private void button14_Click(object sender, EventArgs e)
         {
             if (textBox1.Text != "")
@@ -171,7 +129,6 @@ namespace Calcularor_OLeg
                 znak = true;
             }
         }
-
         private void button15_Click(object sender, EventArgs e)
         {
             if (textBox1.Text != "")
@@ -183,7 +140,6 @@ namespace Calcularor_OLeg
                 znak = true;
             }
         }
-
         private void button16_Click(object sender, EventArgs e)
         {
             if (textBox1.Text != "")
@@ -197,13 +153,40 @@ namespace Calcularor_OLeg
         }
         private void button20_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text != "")
+            {
+                a = Math.Pow(2, float.Parse(textBox1.Text));
+                textBox1.Clear();
+                count = 5;
+                label1.Text = a.ToString();
+                znak = true;
+            }
         }
-
         private void button12_Click(object sender, EventArgs e)
         {
-            calculate();
-            label1.Text = "";
+            if (textBox1.Text != "")
+            {
+                b = double.Parse(textBox1.Text);
+                calculator2 calculator = Class1.cal1(count);
+                result = calculator.calculate(a, double.Parse(textBox1.Text));
+            }
+            else
+            {
+                calculator1 calculator = Class2.cal2(count);
+                b = calculator.calculate(a);
+            }
+            textBox1.Clear();
+            label1.Text = result.ToString();
+            znak = false;
         }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 || e.KeyChar >= 46) && number != 47 && number != 61)
+            {
+                e.Handled = true;
+            }
+        }
+
     }
 }
